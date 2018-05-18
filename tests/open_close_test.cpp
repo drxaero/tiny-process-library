8926b76f49c02785473950dce4ce8d1854dfa42c
Janus Cheng <drxaero@gmail.com>
2018-05-18 20:16:52 +0800

    TKT-014954: NEW_FEATURE - Now the executables built by VS2017 can pass open_close_test.

--- tests/open_close_test.cpp
+++ tests/open_close_test.cpp
@@ -7,12 +7,23 @@ using namespace TinyProcessLib;
 int main() {
   bool stdout_error=false;
   for(size_t c=0;c<10000;c++) {
-    Process process("echo Hello World "+to_string(c), "", [&stdout_error, c](const char *bytes, size_t n) {
-      if(string(bytes, n)!="Hello World "+to_string(c)+"\n")
+#ifdef _WIN32
+    const Process::string_type cmd = L"echo Hello World "+to_wstring(c);
+#else
+    const Process::string_type cmd = "echo Hello World "+to_string(c);
+#endif
+    const Process::string_type arg;
+    Process process(cmd, arg, [&stdout_error, c](const char *bytes, size_t n) {
+#ifdef _WIN32
+    const auto line_end = "\r\n";
+#else
+    const auto line_end = "\n";
+#endif
+      if(string(bytes, n)!="Hello World "+to_string(c)+line_end)
         stdout_error=true;
     }, [](const char *, size_t) {
     }, true);
-    auto exit_status=process.get_exit_status();
+    const auto exit_status=process.get_exit_status();
     if(exit_status!=0) {
       cerr << "Process returned failure." << endl;
       return 1;
